0591ef82c24ceb89e3fb7f1be7fafe463bd893cd
iamovsyanka <anakstkv@gmail.com>
2020-12-10 01:28:20 +0300

    refactor: delete console.log

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,7 +2,6 @@ export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   if (user && user.token) {
-    console.log('Bearer_' + user.token);
     return { Authorization: 'Bearer_' + user.token };
   } else {
     return {};
